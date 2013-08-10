7961ec030b16c556a223b6057d1184c1fe4483fb
Paul Joey Clark <joey@neuralyte.org>
2013-08-11 04:02:55 +0800

    correct perspective, still got flatness bug

--- cube_slider.js
+++ cube_slider.js
@@ -1,31 +1,30 @@
 
 var fiftyPercent = Math.round(window.innerWidth / 2) + 'px';
 
-var pointingRight = 'translateZ(-'+fiftyPercent+') translate(+50%,0%) rotateY(+90deg) translate(-50%,0%)';
-var  pointingLeft = 'translateZ(-'+fiftyPercent+') translate(-50%,0%) rotateY(-90deg) translate(+50%,0%)';
-var pointingFront = 'translateZ(0px) translate(0,0%) rotateY(0deg) translate(0,0%)';
+var pointingRight = 'translateZ(-'+fiftyPercent+') rotateY(+90deg) translateZ(+'+fiftyPercent+')';
+var  pointingLeft = 'translateZ(-'+fiftyPercent+') rotateY(-90deg) translateZ(+'+fiftyPercent+')';
+var pointingFront = 'translateZ(-'+fiftyPercent+') rotateY(0deg) translateZ(+'+fiftyPercent+')';
 
 var superInit = Slider.init;
 Slider.init = function() {
   superInit();
-  Slider.allSlides.show();
+  //Slider.allSlides.show();
   Slider.allSlides.css({
     transform: pointingRight,
-    transition: 'all 1s'
+    transition: 'all 0s'
   });
-  setTimeout(function() {
-    Slider.allSlides.hide();
-  },500);
+  Slider.currentSlide.css({
+    transform: pointingFront,
+    transition: 'all 0s'
+  });
+  //setTimeout(function() {
+    //Slider.allSlides.hide();
+  //},10);
 };
 
 Slider.animateSlideNext = function(currentSlide, nextSlide) {
 
-  currentSlide.css({
-    transform: pointingLeft,
-    transition: 'all 1s'
-  });
-
-  nextSlide.show();
+  //nextSlide.show();
 
   /*
   nextSlide.css({
@@ -35,17 +34,37 @@ Slider.animateSlideNext = function(currentSlide, nextSlide) {
   */
 
   setTimeout(function(){
+    currentSlide.css({
+      transform: pointingLeft,
+      transition: 'all 1s'
+    });
     nextSlide.css({
       transform: pointingFront,
       transition: 'all 1s'
     });
+    setTimeout(function(){
+      currentSlide.hide();
+    },1000);
   },10);
-
-  setTimeout(function(){
-    currentSlide.hide();
-  },1000);
 
 };
 
+Slider.animateSlidePrev = function(currentSlide, prevSlide) {
+
+  setTimeout(function(){
+    currentSlide.css({
+      transform: pointingRight,
+      transition: 'all 1s'
+    });
+    prevSlide.css({
+      transform: pointingFront,
+      transition: 'all 1s'
+    });
+    setTimeout(function(){
+      currentSlide.hide();
+    },1000);
+  },10);
+
+};
 
 
