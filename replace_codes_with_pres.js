10fb4715f891b5e7af3031a92d82eb69f489079b
Paul Joey Clark <joey@neuralyte.org>
2013-09-01 05:33:23 +0800

    do not act on <code> elements mid-sentence

--- replace_codes_with_pres.js
+++ replace_codes_with_pres.js
@@ -1,7 +1,9 @@
-// Replace all large 'code' elements with 'pre' elements because they render better.
-// Note that we don't want to catch 'code' element in the middle of a sentence.  :S
+// Replace all large 'code' elements with 'pre' elements because they render better with the particular markdown I am using.
 $("p > code").each(function(){
   var old = $(this);
-  var better = $("<pre>").text(old.text());
-  old.replaceWith(better);
+  // Note that we don't want to act on any 'code' element in the middle of a sentence (alongside text elements).  We only want those which sit on their own.
+  if (old.parent()[0].childNodes.length == 1) {
+    var better = $("<pre>").text(old.text());
+    old.replaceWith(better);
+  }
 });
