60d939715fa942d9e925ccd9802f833f6f8c54e8
Paul Joey Clark <joey@neuralyte.org>
2013-09-01 06:31:03 +0800

    hide the non-content <p>s

--- group_into_slides.js
+++ group_into_slides.js
@@ -6,3 +6,9 @@ $("h1").each(function(){
   header.replaceWith(newContent);
   newContent.append(contents);
 });
+
+// If there were any gaps in our markdown, e.g. to introduce <style> or <script> tags, these may have been converted into <p>s with no content.  It seems neater to hide them (even though we cannot see them anyway).
+$(document).ready(function() {
+  $(".container > p").hide();
+});
+
